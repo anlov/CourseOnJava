6922f430b0600841be12807a454e676fc96d2c9b
anlov <andrey.orlov.97@gmail.com>
2025-09-16 22:20:52 +0300

    Lesson_17

---
+++ proJect/src/main/java/anlov/java/common/BasePage.java
@@ -0,0 +1,10 @@
+package anlov.java.common;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+
+
+public class BasePage {
+    protected AndroidDriver<AndroidElement> driver = InstanceDriver.getDriver();
+}
